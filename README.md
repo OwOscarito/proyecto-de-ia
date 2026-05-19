5fefbf46a38235f766999b0ce6702dbe1782dcf7
Oscar Garrido <oscar.garrido.o@ug.uchile.cl>
2026-05-19 14:10:58 -0400

    init

---
+++ README.md
@@ -0,0 +1,32 @@
+# Preparar env
+
+## venv
+
+```sh
+python -m venv venv
+source venv/bin/activate
+pip install requirements.txt
+```
+
+## Conda
+
+```sh
+conda create -f conda-env.yml
+conda activate proyecto-de-ia
+```
+
+# Ejecutar
+
+# venv / conda
+
+```sh
+flask run frontend
+flask run backend
+```
+
+# uv
+
+```sh
+uv run flask run frontend
+uv run flask run backend
+```
---
+++ frontend/__init__.py
@@ -0,0 +1,22 @@
+import os
+
+from dotenv import load_dotenv
+from flask import Flask
+
+load_dotenv()
+
+secret_key = os.getenv("SECRET_KEY")
+
+app = Flask(__name__)
+app.secret_key = secret_key
+
+
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World!</p>"
+
+
+if __name__ == "__main__":
+    port = os.getenv("FRONTEND_PORT")
+    port = int(port) if port else None
+    app.run(port=port)
